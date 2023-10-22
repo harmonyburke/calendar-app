6457e9980c20e5bd7e503b62905528decaa9e954
Harmony Burke <theharmonyburke@gmail.com>
2023-10-22 16:34:38 -0500

    changed logic for color changing

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-
-
-  var save=$(".btn");
-  // var for save button
- 
 $(document).ready(function() {
   // this function specifies this all to only run when the HTML is fully loaded and the correct conditions are met. 
   // Everything within this function will run on page load 
+  
+    var save=$(".btn");
+    // var for save button
 
 save.on("click", function(){
   // this function runs when the save button is clicked
@@ -46,67 +41,27 @@ save.on("click", function(){
 });
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
   
-  var allHours = [
-    // array for all the hour slots
-    // "hour-7",
-    // "hour-8",
-    // "hour-9",
-    // "hour-10",
-    // "hour-11",
-    // "hour-12",
-    // "hour-13",
-    // "hour-14",
-    // "hour-15",
-    // "hour-16",
-    // "hour-17",
-    // "hour-18",
-    // "hour-19",
-    // "hour-20",
-    // "hour-21"
-    $(".container")
-  ]
-  var hour=allHours
-  
-  
-  
-  for (var i=0; i<allHours.length;i++){
-    // loops through all of the hours 
-
-    var currentTime=dayjs();
-    console.log(currentTime)
-    $("#time").text(currentTime.format("hh:mm a"));
-    // adds the time to the top of the page
-
-    // gets the time of day
-
-    if (hour===currentTime) {
-      $(".hour").eq(i).toggleClass(".present")
-    }else if (hour>currentTime){
-      $(".hour").eq(i).toggleClass(".future")
-    }else if (hour<currentTime){
-      $(".hour").eq(i).toggleClass(".past")
-      // checks to see if hour is greater than, less than, or equal to the current time and changes the color based on the answer
-
+  function styleHour(){
+    var currentHour=dayjs().hour()
+    // gets the current hour
+    console.log(currentHour)
+    $(".time-block").each(function(){
+      var perHour=parseInt($(this).attr("id").split("-")[1]);
+      // takes the id and splits it into an array (ex.hour, 7) and then splits the array after the "-" and calls for the second entry in the array and parses all of this into a number instead of a string
+      console.log(perHour)
+      if (perHour===currentHour) {
+        $(this).addClass(".present")
+      }else if (perHour>currentHour){
+        $(this).addClass(".future")
+      }else if (perHour<currentHour){
+        $(this).addClass(".past")
     }
+
+    })
   }
+
+  styleHour()
 // The above code will loop through all time-block elements, compare their associated hour with the current time,
 // and change the background color if they match.
 
@@ -131,7 +86,7 @@ save.on("click", function(){
   
   
   
-  var timeEl=$("#time")
+  
   var today=dayjs();
   $("#currentDay").text(today.format("MMM, DD,YYYY"));
   // this function calls in dayjs and adds the current date to the id "currentDay" with the given format.
